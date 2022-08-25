59bd4608c27b7d7c3ac05540a9cb1559f0960b10
Jacob Lockhart <procoder165@gmail.com>
2022-08-25 00:54:11 -0400

    Messing around with if statements

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function time(){
 var now = moment();
-nowHour = 5;
-
+nowHour = 10;
 // var nowHour = now.format("h");
+var amCheck = now.calendar();
 
-for(var i = 9; i < 17; i++){
-    if (i < nowHour){
+for(var i = 0; i < 17; i++){
+    if (nowHour == i){
+        $("#" + i).addClass("bg-success");
+    }
+    else if (i <= 12 && i >= 9 && nowHour != i){
+       
         $("#" + i).addClass("bg-secondary");
     }
+    else if (i <= 5 && i >= 1 && nowHour != i){
+        $("#" + i).addClass("bg-secondary");
+    }
+    else{
+        $("#" + i).addClass("bg-primary");
+    }
+    
 }
+
+
 
 
 }
