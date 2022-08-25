56ca30cd35440de80423661c4bc5f1d5a1303ffc
Jacob Lockhart <procoder165@gmail.com>
2022-08-25 02:28:05 -0400

    Got colors to work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function time(){
 var now = moment();
 var amCheck ="AM"
-var nowHour = "12";
+var nowHour = 5;
 
 
 var index = [9, 10, 11, 12, 1, 2, 3, 4, 5];
@@ -11,11 +11,11 @@ var low = [1, 2, 3, 4, 5];
 // var nowHour = now.format("h");
 nowHour = parseInt(nowHour);
 
-console.log(nowHour)
+
 
 // $("#" + i).addClass(""); 
 
-for(var i = 0; i < 9; i++){
+for(var i = 0; i < 10; i++){
     // Make everything Primary
     $("#" + index[i]).addClass("bg-primary");
 }
@@ -25,14 +25,44 @@ for(var i = 0; i < 9; i++){
 
 for(var i = 0; i < high.length; i++){
     // Select what has passed
-     if(high[i] <  nowHour){
-        console.log("True")
+     if(high[i] >  nowHour ){
+        console.log("Hello")
+        $("#" + high[i]).addClass("bg-primary"); 
+    }
+
+    else if(high[i] < nowHour ){
         $("#" + high[i]).addClass("bg-secondary");
     }
-    else{
-        $("#" + index[i]).addClass("bg-primary");
-    }
 }
+
+if (9 > nowHour){
+    $("#1").addClass("bg-primary")
+    $("#2").addClass("bg-primary")
+    $("#3").addClass("bg-primary")
+    $("#4").addClass("bg-primary")
+    $("#5").addClass("bg-primary")
+    $("#9").addClass("bg-secondary")
+    $("#10").addClass("bg-secondary")
+    $("#11").addClass("bg-secondary")
+    $("#12").addClass("bg-secondary")
+
+    for(var i = 0; i < low.length; i++){
+    
+
+
+        if (low[i] > nowHour){
+           console.log("True")
+           $("#" + low[i]).addClass("bg-primary");
+       }
+       else if(low[i] < nowHour){
+           console.log(false)
+           $("#" + low[i]).addClass("bg-secondary");
+       }
+   }
+   
+}
+
+
 
 
 
@@ -40,9 +70,6 @@ for(var i = 0; i < high.length; i++){
 $("#" + nowHour).addClass("bg-success");
 
 
-console.log(high.includes(nowHour))
-console.log(low.includes(nowHour))
-console.log(index.includes(nowHour))
 
 }
 
