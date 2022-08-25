1b74b06a69580f916667d25b3010bda333c78253
Jacob Lockhart <procoder165@gmail.com>
2022-08-25 01:42:30 -0400

    Have a plan together

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 function time(){
 var now = moment();
-nowHour = 10;
-// var nowHour = now.format("h");
-var amCheck = now.calendar();
+var amCheck ="AM"
+var nowHour = "10";
 
-for(var i = 0; i < 17; i++){
+
+var index = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+var high = ["9", "10", "11", "12"];
+var low = ["1", "2", "3", "4", "5"];
+// var amCheck = now.calendar();
+// var nowHour = now.format("h");
+console.log(nowHour)
+
+// $("#" + i).addClass(""); 
+
+for(var i = 0; i < 13; i++){
+    // Changes current time bg
     if (nowHour == i){
-        $("#" + i).addClass("bg-success");
+        $("#" + nowHour).addClass("bg-success");
     }
-    else if (i <= 12 && i >= 9 && nowHour != i){
-       
-        $("#" + i).addClass("bg-secondary");
+    // Select what has passed
+    else if(high.includes(nowHour)){
+        $("#" + high[i]).addClass("bg-secondary");
     }
-    else if (i <= 5 && i >= 1 && nowHour != i){
-        $("#" + i).addClass("bg-secondary");
+    else if (low.includes(nowHour)){
+        $("#" + low[i - 2]).addClass("bg-secondary");
     }
-    else{
-        $("#" + i).addClass("bg-primary");
-    }
+    // Make rest Primary
+    $("#" + index[i]).addClass("bg-primary");
     
+
 }
 
-
-
+console.log(high.includes(nowHour))
+console.log(low.includes(nowHour))
+console.log(index.includes(nowHour))
 
 }
 
